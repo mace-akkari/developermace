21e8e733933e6f7534dad963553e7a48bed94ee4
Mace Akkari <maceplus@gmail.com>
2025-06-26 14:37:50 +0100

    chore: add folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import type React from "react"
 import type { Metadata } from "next";
+import { Inter } from "next/font/google"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -6,14 +8,16 @@ export const metadata: Metadata = {
   description: "Mace Akkari Portfolio",
 };
 
+const inter = Inter({ subsets: ["latin"] })
+
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
